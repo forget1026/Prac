363ce2445b8e1654461bfa004307f487384a00f2
Jaehak <dma963@naver.com>
2020-03-06 15:07:33 +0900

    배포 테스트

--- day4/back/backend/src/test/java/day4/back/backend/DemoApplicationTests.java
+++ day4/back/backend/src/test/java/day4/back/backend/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package day4.back.backend;
+// package day4.back.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+// @SpringBootTest
+// class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
